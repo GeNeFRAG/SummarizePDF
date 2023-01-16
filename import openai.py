7a892cdb6699497c2c42c0276c744be0c33608f5
GeNeFRAG <froehlich.gerhard@gmail.com>
2023-01-16 15:42:08 +0100

    Reading URL from sys

--- import openai.py
+++ import openai.py
@@ -3,6 +3,7 @@ import wget
 import pathlib
 import pdfplumber
 import numpy as np
+import sys
 
 def getPaper(paper_url, filename="random_paper.pdf"):
     """
@@ -14,25 +15,34 @@ def getPaper(paper_url, filename="random_paper.pdf"):
 
     return downloadedPaperFilePath
 
-
 def showPaperSummary(paperContent):
     tldr_tag = "\n tl;dr:"
-    openai.organization = 'Enter your organisation'
-    openai.api_key = "Enter your API Key"
+    openai.organization = ''
+    openai.api_key = "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"
     model_list = openai.Model.list() 
     
     for page in paperContent:    
         text = page.extract_text() + tldr_tag
+        text = "Analyse and Summarize following text in short sentences: " + text
+    
         response = openai.Completion.create(model="text-davinci-003",prompt=text,temperature=0.3,
             max_tokens=2048,
             top_p=1,
             frequency_penalty=0.2,
             presence_penalty=0.2,
+            echo=False,
             stop=["\n"]
         )
-        print(page.page_number)
+        
+        #print(page.page_number)
         print(response["choices"][0]["text"])
 
-paperFilePath = getPaper("https://eur-lex.europa.eu/legal-content/EN/TXT/PDF/?uri=CELEX:52020PC0595&from=EN")
+
+
+if len(sys.argv) == 1:
+    raise Exception("URL to PDF missing")
+url=sys.argv[1]
+
+paperFilePath = getPaper(url)
 paperContent = pdfplumber.open(paperFilePath).pages
 showPaperSummary(paperContent)
