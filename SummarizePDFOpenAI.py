df9cb363c151ffe5402cc9fb02789782f92ddbe5
GeNeFRAG <froehlich.gerhard@gmail.com>
2023-01-16 17:07:23 +0100

    Configurable apikey with TOML

--- SummarizePDFOpenAI.py
+++ SummarizePDFOpenAI.py
@@ -4,11 +4,12 @@ import pathlib
 import pdfplumber
 import numpy as np
 import sys
+import tomli
 
 def getPaper(paper_url, filename="random_paper.pdf"):
     """
     Downloads a paper from it's arxiv page and returns
-    the local path to that file.
+    the local pathpip to that file.
     """
     downloadedPaper = wget.download(paper_url, filename)    
     downloadedPaperFilePath = pathlib.Path(downloadedPaper)
@@ -17,8 +18,6 @@ def getPaper(paper_url, filename="random_paper.pdf"):
 
 def showPaperSummary(paperContent):
     tldr_tag = "\n tl;dr:"
-    openai.organization = ''
-    openai.api_key = ""
     model_list = openai.Model.list() 
     
     for page in paperContent:    
@@ -37,8 +36,13 @@ def showPaperSummary(paperContent):
         #print(page.page_number)
         print(response["choices"][0]["text"])
 
+#Reading out OpenAI API keys and organisation
+with open("openai.toml","rb") as f:
+    data = tomli.load(f)
+    openai.api_key=data["apikey"]
+    openai.organization=data["organization"]
 
-
+#Getting PDF URl from command line
 if len(sys.argv) == 1:
     raise Exception("URL to PDF missing")
 url=sys.argv[1]
