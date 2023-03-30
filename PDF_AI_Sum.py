715c00edad05cb8f2a5d8ffdaa5f5272c6e643b9
GeNeFRAG <froehlich.gerhard@gmail.com>
2023-03-30 13:42:15 +0200

    Import sorting

--- PDF_AI_Sum.py
+++ PDF_AI_Sum.py
@@ -1,10 +1,12 @@
-import openai
-import wget
 import pathlib
-import pdfplumber
-import numpy as np
 import sys
+
+import numpy as np
+import openai
+import pdfplumber
 import tomli
+import wget
+
 
 # This function downloads a paper from the provided URL and saves it with the provided filename or a default filename of "random_paper.pdf". It then returns the path to the downloaded paper. If an error occurs when downloading the paper, it prints an error message and returns None. 
 def getPaper(paper_url, filename):
