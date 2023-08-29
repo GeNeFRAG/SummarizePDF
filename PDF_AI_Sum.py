8c71669def7d8474a375597b3a0f9d68d8bb727d
GeNeFRAG <froehlich.gerhard@gmail.com>
2023-08-29 14:24:21 +0200

    Now utilizing my GPTCommons module

--- PDF_AI_Sum.py
+++ PDF_AI_Sum.py
@@ -12,6 +12,20 @@ SPECIAL_CHARACTERS = string.punctuation + "“”‘’"
 PATTERN = re.compile(r'[\n\s]+')
 
 def clean_text(text):
+    """
+    Cleans a given text by replacing line breaks, consecutive whitespace, and handling special characters.
+
+    Args:
+    text (str): The input text to be cleaned.
+
+    Returns:
+    str: The cleaned text.
+
+    Example:
+    >>> dirty_text = "This is a\ndirty    text!!"
+    >>> clean_text(dirty_text)
+    'This is a dirty text  '
+    """
    # Replace line breaks and consecutive whitespace with a single space
     text = re.sub(PATTERN, ' ', text).strip()
     
@@ -21,6 +35,24 @@ def clean_text(text):
     return text
 
 def get_completion(prompt, model, temperature=0):
+    """
+    Retrieves a completion using the OpenAI ChatCompletion API with the specified model and parameters.
+
+    Args:
+    prompt (str): The user's input or prompt for generating the completion.
+    model (str): The OpenAI model identifier (e.g., "gpt-3.5-turbo").
+    temperature (float, optional): The degree of randomness in the model's output (default is 0).
+                                   A higher value makes the output more random, while a lower value makes it more deterministic.
+
+    Returns:
+    str: The generated completion text.
+
+    Example:
+    >>> user_prompt = "Translate the following English text to French: 'Hello, how are you?'"
+    >>> model_id = "gpt-3.5-turbo"
+    >>> get_completion(user_prompt, model_id, temperature=0.7)
+    'Bonjour, comment ça va ?'
+    """
     messages = [{"role": "user", "content": prompt}]
     response = openai.ChatCompletion.create(
         model=model,
@@ -31,10 +63,23 @@ def get_completion(prompt, model, temperature=0):
 
 def get_arg(arg_name, default=None):
     """
-    Safely reads a command line argument by name.
-    :param arg_name: the name of the argument to read.
-    :param default: the default value to return if the argument is not found.
-    :return: the value of the argument if found, or the default value.
+    Retrieves the value of a command-line argument by its name from the sys.argv list.
+
+    Args:
+    arg_name (str): The name of the command-line argument to retrieve.
+    default: The default value to return if the argument is not found (default is None).
+
+    Returns:
+    str or default: The value of the specified command-line argument or the default value if not found.
+
+    Example:
+    >>> # Assuming the command-line arguments are ['--lang', 'English', '--url', 'example.com']
+    >>> get_arg('--lang', 'Spanish')
+    'English'
+    >>> get_arg('--url', 'localhost')
+    'example.com'
+    >>> get_arg('--port', 8080)
+    8080
     """
     if "--help" in sys.argv:
         print("Usage: python PD_AI_Sum.py [--help] [--lang] [--url] [--ofile]")
@@ -52,8 +97,26 @@ def get_arg(arg_name, default=None):
     except (IndexError, ValueError):
         return default
 
-# This function downloads a paper from the provided URL and saves it with the provided filename or a default filename of "random_paper.pdf". It then returns the path to the downloaded paper. If an error occurs when downloading the paper, it prints an error message and returns None. 
 def download_paper(paper_url, filename):
+    """
+    Downloads a paper from the provided URL and saves it with the specified filename or a default filename.
+
+    Args:
+    paper_url (str): The URL of the paper to be downloaded.
+    filename (str, optional): The desired filename to save the paper as (default is None).
+
+    Returns:
+    pathlib.Path or None: The path to the downloaded paper file as a pathlib.Path object, or None if download fails.
+
+    Example:
+    >>> paper_url = "https://example.com/paper.pdf"
+    >>> downloaded_paper = download_paper(paper_url, "research_paper.pdf")
+    >>> print(downloaded_paper)
+    '/path/to/research_paper.pdf'
+
+    Note:
+    This function uses the 'wget' library to download the paper from the URL. If the download fails, it returns None.
+    """
     try:
         # Download the paper from the provided url, with the provided filename or default filename
         downloadedPaper = wget.download(paper_url, filename)    
@@ -66,8 +129,25 @@ def download_paper(paper_url, filename):
 
     return downloadedPaperFilePath
 
-# This function takes in a paperContent and prints out a summary of the paper. It first checks if the paperContent is None and returns if it is. It then creates a tldr tag to be added at the end of each summary It then calls the OpenAI API to generate a summary with certain parameters such as temperature, max_tokens, top_p, frequency_penalty, presence_penalty, echo and stop. Finally it prints out the generated summary. 
 def show_page_summary(paperContent):
+    """
+    Generates a summary of a PDF document's pages, removes duplicate or redundant information, and prints the result.
+
+    Args:
+    paper_content (list): A list of pages from the PDF document.
+
+    Returns:
+    None
+
+    Example:
+    >>> pdf_pages = [page1, page2, page3, ...]
+    >>> show_page_summary(pdf_pages)
+    [Summary of the PDF document's pages]
+    [Cleaned text with duplicate/redundant information removed]
+
+    Note:
+    The function relies on the 'clean_text' and 'get_completion' functions, and it uses a specific model ('gptmodel') and language ('lang') for text generation.
+    """
     if paperContent is None:
         return
     try:
